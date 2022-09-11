2faa3930cabbcc28c32afb50811db613c802c341
Tsahoika Hleb <hlebtsahoika@icloud.com>
2022-09-11 21:37:12 +0300

    Complete design and  functionality.

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Hleb Tsahoika on 17.07.22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var heightLabel: UILabel!
+    
+    @IBOutlet var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value) + "kg"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 
